a3022c67d420027d425c957d519098c779c32257
Ayivi <kafuigalley7@gmail.com>
2022-12-08 14:48:01 -0800

    6 size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#!/bin/bash
+
 #include <stdio.h>
 /**
  * main - Entry point
  *
  * Description: prints "Programming is like building a multilingual puzzle"
- * 
- *Return: 0
+ *
+ * Return: Always 0 (Success) 
  */
+
 int main(void)
 {
 	 puts("\"Programming is like building a multilingual puzzle");
