204d2f63aefc4cfa9733646fe12714fd4433d703
Ayivi <kafuigalley7@gmail.com>
2022-12-10 10:22:20 -0800

    8printbase16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 	int num;
 	char lc;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num <= 9; num++)
 		putchar((num % 10) + '0');
 	for (lc = 'a'; lc <= 'f'; lc++)
 		putchar(lc);
