b7f89814c5642de58f83f7e85a0a7e210fac6f14
Ayivi <kafuigalley7@gmail.com>
2022-12-13 08:46:38 -0800

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,22 @@
 #include "main.h"
 /**
+ *print_alphabet_x10 - print alphabet 10x
  *
+ * Return: 0 Always(success)
+ */
+void print_alphabet_x10(void)
+{
+	int a, ab;
+
+	ab = 0;
+
+	while (ab < 10)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		ab++;
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			{
 				_putchar(' ');
 			}
-			_putchar((d % 10) + '0');
+			_putchar((c % 10) + '0');
 
 			if (b < 9)
 			{
