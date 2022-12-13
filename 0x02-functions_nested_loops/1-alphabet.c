4d64daea273b58cd5092fc240a5c306359a6fdbe
Ayivi <kafuigalley7@gmail.com>
2022-12-13 08:31:47 -0800

    times table

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,6 +13,6 @@ void print_alphabet(void)
 		_putchar(c);
 		c++;
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
 
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * times_table - print the 9 times table
+ * return: 0 Always (success)
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; b <= 9; a++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (b = 1; b <= 9; b++)
+		{
+			c = (a * b);
+			if ((c / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (b < 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
