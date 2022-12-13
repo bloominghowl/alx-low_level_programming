e73d82afd86a1d39844052ae5fd47c482b2efc5f
Ayivi <kafuigalley7@gmail.com>
2022-12-13 07:35:16 -0800

    lc alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: 0 Always (success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n')
+}
+
