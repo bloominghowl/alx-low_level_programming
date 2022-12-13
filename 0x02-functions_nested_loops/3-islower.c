1389b60595618154cd54a14cbeb62d75ae479462
Ayivi <kafuigalley7@gmail.com>
2022-12-13 09:19:18 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: single letter input
+ * Return: 1 if int a is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (a >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
