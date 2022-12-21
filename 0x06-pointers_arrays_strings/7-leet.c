4be382131a1cec71dff5569461bc0620f4cb3959
Ayivi <kafuigalley7@gmail.com>
2022-12-21 08:18:36 -0800

    encoding string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - function to encode string into 1337
+ * @s: string to be crypted
+ *
+ * Return: pointer to s
+ */
+
+char *leet(char *s)
+{
+	char *m = "aeotl";
+	char *M = "AEOTL";
+	int x[] = {'4', '3', '0', '7', '1'};
+	int i;
+	char *p = s;
+
+	while (*s != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == *(m + i) || *s == *(M + i))
+				*s = x[i];
+		}
+		s++;
+	}
+	return (p);
+}
