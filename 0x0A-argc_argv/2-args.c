14a9c4220e0862d226b7554a9e621a797140aa9c
Ayivi <kafuigalley7@gmail.com>
2023-01-09 11:20:03 -0800

    five minutes

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
