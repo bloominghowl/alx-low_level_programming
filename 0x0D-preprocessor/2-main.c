c0852217a25238fc963f350572cf0054b062ee16
Ayivi <kafuigalley7@gmail.com>
2023-01-16 05:29:34 -0800

    h file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,12 @@
 
 /**
  * main - print the name of the file it was compiled from
+ * using common predefined macros
  *
  * Return: 0 if success, 1 if error
  */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", FILE);
 	return (0);
 }
