80eca236216d0001bc2f55cd3bf26b37c81a0492
Ayivi <kafuigalley7@gmail.com>
2023-02-02 10:13:49 -0800

    get endianness

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,24 +12,20 @@
  
 void rev_bin(unsigned long int n, int check)
 {
-if (check == 1)
-{
-_putchar('0');
-return;
+	if (check == 1)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 0)
+		return;
+	rev_bin(n >> 1, check);
+
+	if ((n & 1) == 0)
+		_putchar('0');
+	if ((n & 1) == 1)
+		_putchar('1');
 }
-
-if (n == 0)
-return;
-
-rev_bin(n >> 1, check);
-
-if ((n & 1) == 0)
-_putchar('0');
-
-if ((n & 1) == 1)
-_putchar('1');
-}
-
 /**
 * print_binary - prints the binary representation of a number
 * @n: base 10 number
@@ -38,8 +34,8 @@ _putchar('1');
 */
 void print_binary(unsigned long int n)
 {
-if (n == 0)
-rev_bin(n, 1);
-else
-rev_bin(n, 0);
+	if (n == 0)
+		rev_bin(n, 1);
+	else
+		rev_bin(n, 0);
 }
