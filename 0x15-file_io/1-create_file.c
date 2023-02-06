b76cca80256afd43bf9173bd971c352d157bd9de
Ayivi <kafuigalley7@gmail.com>
2023-02-06 08:44:50 -0800

    create text file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,25 +16,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
+	int fd, checkw, l = 0;
 
-	if (filename == NULL)
+	if (filename == 0)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-	while (text_content[i] != '\0')
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
 	{
-		i++;
+		while (text_content[l] != 0)
+			l++;
+		checkw = write(fd, text_content, l);
+
+		if (checkw == -1)
+			return (-1);
 	}
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (file == -1)
-		return (-1);
-
-	write(file, text_content, i);
-
-	return (i);
+	close(fd);
+	return (1);
 }
