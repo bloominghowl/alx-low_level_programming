7d587870f201e48979f56c5e4d15353225fd2f72
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-05-24 12:51:22 +0000

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
+""" Island Perimeter """
+
 
 def island_perimeter(grid):
-    """
-    Calculates the perimeter of the island described in the grid.
-
-    Args:
-        grid (list[list[int]]): A list of lists representing the grid.
-
-    Returns:
-        int: The perimeter of the island.
-
-    """
-    if not grid or not grid[0]:
-        return 0
-
-    rows = len(grid)
-    cols = len(grid[0])
-
-    perimeter = 0
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4  # Add 4 to the perimeter for each land cell
-
-                # Check neighboring cells
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract 2 if there is a neighboring land cell to the top
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract 2 if there is a neighboring land cell to the left
-
-    return perimeter
+    """ Island Perimeter """
+    peri = 0
+    gl = []
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            gl.append(grid[i][j])
+    for i in range(len(gl)):
+        if i < len(gl) - 1 and i > 0:
+            if gl[i] == 0 and gl[i + 1] == 1 or gl[i] == 0 and gl[i - 1] == 1:
+                peri += 1
+    if peri % 2 == 0:
+        peri *= 2
+    else:
+        peri = (peri * 2) - 1
+    return peri
