270ea3315f601182985105ba04facf66da5b79b7
Ayivi <kafuigalley7@gmail.com>
2023-07-10 02:49:42 -0700

    intial push

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches for a value in an array using linear search
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: The index where the value is located, or -1 if not found or array is NULL
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return -1;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        if (array[i] == value)
+            return i;
+    }
+
+    return -1; // Value not found
+}
